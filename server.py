fb086c8b03da31cd5965ad7335d78fd95c8f54e3
Jaswanth Karangula <jaswanth@Jaswanths-MacBook-Pro.local>
2022-11-24 10:52:04 -0800

    Simple Flask Model

--- server.py
+++ server.py
@@ -1,15 +1,10 @@
 from flask import Flask, redirect, render_template, jsonify, request
-import cv2, os, sys, imutils
-import numpy as np
-import tensorflow as tf
 
-MODEL_PATH = 'mnist_model'
+
+MODEL_PATH = 'saved_models/simple_model'
 
 app = Flask(__name__)
-app.config['DEBUG'] = False
 
-# Load the Model
-model = tf.keras.models.load_model(MODEL_PATH)
 
 @app.route('/', methods=['GET'])
 def hello_world():
@@ -20,21 +15,35 @@ def predict():
     imagefile = request.files['imagefile']
     imagepath = "./images/"+imagefile.filename
     imagefile.save(imagepath)
-    return render_template('index.html',prediction = "Predicted")
-    
+    prediction_result = model_classification(imagepath)
+    return render_template('index.html',prediction = prediction_result)
 
-@app.route('/infer', methods=['POST'])
-def hand_classifier():
-    # Receive the encoded frame and convert it back to a Numpy Array
-    encoded_image = np.frombuffer(request.data, np.uint8)
-    image = cv2.imdecode(encoded_image, -1) # Decode image without converting to BGR
-    
-    image = np.expand_dims([image], axis=-1) # Add dimensions to create appropriate tensor shapes
-    
-    # Run inference on the frame
-    hand = model.predict(image)
 
-    return jsonify(str(np.argmax(hand))) # Because only string can be converted to JSON
+def model_classification(imagepath):
+    from numpy import asarray
+    from PIL import Image
+    import tensorflow as tf
+    import numpy as np
+    batch_size = 32
+    img_height = 128 #180
+    img_width = 128 #180
+    IMAGE_SIZE = (img_height, img_width)
+    newsaved_model = tf.keras.models.load_model('saved_models/simple_model/')
+    class_names = ['Apple Braeburn', 'Apple Crimson Snow', 'Apple Golden 1', 'Apple Golden 2', 'Apple Golden 3', 'Apple Granny Smith', 'Apple Pink Lady', 'Apple Red 1', 'Apple Red 2', 'Apple Red 3', 'Apple Red Delicious', 'Apple Red Yellow 1', 'Apple Red Yellow 2', 'Apricot', 'Avocado', 'Avocado ripe', 'Banana', 'Banana Lady Finger', 'Banana Red', 'Beetroot', 'Blueberry', 'Cactus fruit', 'Cantaloupe 1', 'Cantaloupe 2', 'Carambula', 'Cauliflower', 'Cherry 1', 'Cherry 2', 'Cherry Rainier', 'Cherry Wax Black', 'Cherry Wax Red', 'Cherry Wax Yellow', 'Chestnut', 'Clementine', 'Cocos', 'Corn', 'Corn Husk', 'Cucumber Ripe', 'Cucumber Ripe 2', 'Dates', 'Eggplant', 'Fig', 'Ginger Root', 'Granadilla', 'Grape Blue', 'Grape Pink', 'Grape White', 'Grape White 2', 'Grape White 3', 'Grape White 4', 'Grapefruit Pink', 'Grapefruit White', 'Guava', 'Hazelnut', 'Huckleberry', 'Kaki', 'Kiwi', 'Kohlrabi', 'Kumquats', 'Lemon', 'Lemon Meyer', 'Limes', 'Lychee', 'Mandarine', 'Mango', 'Mango Red', 'Mangostan', 'Maracuja', 'Melon Piel de Sapo', 'Mulberry', 'Nectarine', 'Nectarine Flat', 'Nut Forest', 'Nut Pecan', 'Onion Red', 'Onion Red Peeled', 'Onion White', 'Orange', 'Papaya', 'Passion Fruit', 'Peach', 'Peach 2', 'Peach Flat', 'Pear', 'Pear 2', 'Pear Abate', 'Pear Forelle', 'Pear Kaiser', 'Pear Monster', 'Pear Red', 'Pear Stone', 'Pear Williams', 'Pepino', 'Pepper Green', 'Pepper Orange', 'Pepper Red', 'Pepper Yellow', 'Physalis', 'Physalis with Husk', 'Pineapple', 'Pineapple Mini', 'Pitahaya Red', 'Plum', 'Plum 2', 'Plum 3', 'Pomegranate', 'Pomelo Sweetie', 'Potato Red', 'Potato Red Washed', 'Potato Sweet', 'Potato White', 'Quince', 'Rambutan', 'Raspberry', 'Redcurrant', 'Salak', 'Strawberry', 'Strawberry Wedge', 'Tamarillo', 'Tangelo', 'Tomato 1', 'Tomato 2', 'Tomato 3', 'Tomato 4', 'Tomato Cherry Red', 'Tomato Heart', 'Tomato Maroon', 'Tomato Yellow', 'Tomato not Ripened', 'Walnut', 'Watermelon']
+    # load image
+    image_path = imagepath
+    image = Image.open(image_path)
+    im_resized = image.resize((img_width, img_height))
+    pixels = asarray(im_resized)
+    img_array = tf.expand_dims(pixels, 0) # Create a batch
+    predictions = newsaved_model.predict(img_array)
+    prediction_prob=tf.nn.softmax(predictions)
+    objid=np.argmax(prediction_prob[0], axis=-1)
+    final_class = class_names[objid]
+    final_class_prob = prediction_prob[0][objid].numpy()
+    return final_class+'With Probability (%.2f%%)' %(final_class_prob)
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='3000', debug=True)
