9480d71f7e81252771cc800343c1b9bae43921e7
Jaswanth Karangula <jaswanth@Jaswanths-MacBook-Pro.local>
2022-12-08 12:36:18 -0800

    Testing

--- server.py
+++ server.py
@@ -5,18 +5,28 @@ MODEL_PATH = 'saved_models/simple_model'
 
 app = Flask(__name__)
 
+models = ['Sequential' , 'ResNet50+ Transfer Learning', 'ResNet50- Transfer Learning-FineTuning', 'VGG16+ Transfer Learning', 'VGG16- Transfer Learning-FineTuning']
+model_paths = ['saved_models/simple_model','saved_models/resnet50model_transferlearning','saved_models/resnet50model_finetune','saved_models/vgg16model_transferlearning','saved_models/vgg16model_finetune']
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', models=models)
 
 @app.route('/', methods=['POST'])
 def predict():
     imagefile = request.files['imagefile']
+    model_touse = request.form["model"]
+    index_ = models.index(model_touse)
+    model_path = model_paths[index_]
     imagepath = "./images/"+imagefile.filename
     imagefile.save(imagepath)
-    prediction_result = model_classification(imagepath)
-    return render_template('index.html',prediction = prediction_result)
+    prediction_result = model_classification(imagepath,model_path)
+    return render_template('index.html',prediction = prediction_result,models=models)
+
+@app.route('/dropdown', methods=['GET'])
+def dropdown():
+    models = ['VGG16', 'ResNet50', 'Sequential']
+    return render_template('index.html', models=models)
 
 
 def model_classification(imagepath):
@@ -41,7 +51,31 @@ def model_classification(imagepath):
     objid=np.argmax(prediction_prob[0], axis=-1)
     final_class = class_names[objid]
     final_class_prob = prediction_prob[0][objid].numpy()
-    return final_class+'  With Probability (%.2f)' %(final_class_prob)
+    return final_class #+'  With Probability (%.2f)' %(final_class_prob)
+
+def model_classification(imagepath,model_path):
+    from numpy import asarray
+    from PIL import Image
+    import tensorflow as tf
+    import numpy as np
+    batch_size = 32
+    img_height = 128 #180
+    img_width = 128 #180
+    IMAGE_SIZE = (img_height, img_width)
+    newsaved_model = tf.keras.models.load_model(model_path)
+    class_names = ['Apple Braeburn', 'Apple Crimson Snow', 'Apple Golden 1', 'Apple Golden 2', 'Apple Golden 3', 'Apple Granny Smith', 'Apple Pink Lady', 'Apple Red 1', 'Apple Red 2', 'Apple Red 3', 'Apple Red Delicious', 'Apple Red Yellow 1', 'Apple Red Yellow 2', 'Apricot', 'Avocado', 'Avocado ripe', 'Banana', 'Banana Lady Finger', 'Banana Red', 'Beetroot', 'Blueberry', 'Cactus fruit', 'Cantaloupe 1', 'Cantaloupe 2', 'Carambula', 'Cauliflower', 'Cherry 1', 'Cherry 2', 'Cherry Rainier', 'Cherry Wax Black', 'Cherry Wax Red', 'Cherry Wax Yellow', 'Chestnut', 'Clementine', 'Cocos', 'Corn', 'Corn Husk', 'Cucumber Ripe', 'Cucumber Ripe 2', 'Dates', 'Eggplant', 'Fig', 'Ginger Root', 'Granadilla', 'Grape Blue', 'Grape Pink', 'Grape White', 'Grape White 2', 'Grape White 3', 'Grape White 4', 'Grapefruit Pink', 'Grapefruit White', 'Guava', 'Hazelnut', 'Huckleberry', 'Kaki', 'Kiwi', 'Kohlrabi', 'Kumquats', 'Lemon', 'Lemon Meyer', 'Limes', 'Lychee', 'Mandarine', 'Mango', 'Mango Red', 'Mangostan', 'Maracuja', 'Melon Piel de Sapo', 'Mulberry', 'Nectarine', 'Nectarine Flat', 'Nut Forest', 'Nut Pecan', 'Onion Red', 'Onion Red Peeled', 'Onion White', 'Orange', 'Papaya', 'Passion Fruit', 'Peach', 'Peach 2', 'Peach Flat', 'Pear', 'Pear 2', 'Pear Abate', 'Pear Forelle', 'Pear Kaiser', 'Pear Monster', 'Pear Red', 'Pear Stone', 'Pear Williams', 'Pepino', 'Pepper Green', 'Pepper Orange', 'Pepper Red', 'Pepper Yellow', 'Physalis', 'Physalis with Husk', 'Pineapple', 'Pineapple Mini', 'Pitahaya Red', 'Plum', 'Plum 2', 'Plum 3', 'Pomegranate', 'Pomelo Sweetie', 'Potato Red', 'Potato Red Washed', 'Potato Sweet', 'Potato White', 'Quince', 'Rambutan', 'Raspberry', 'Redcurrant', 'Salak', 'Strawberry', 'Strawberry Wedge', 'Tamarillo', 'Tangelo', 'Tomato 1', 'Tomato 2', 'Tomato 3', 'Tomato 4', 'Tomato Cherry Red', 'Tomato Heart', 'Tomato Maroon', 'Tomato Yellow', 'Tomato not Ripened', 'Walnut', 'Watermelon']
+    # load image
+    image_path = imagepath
+    image = Image.open(image_path)
+    im_resized = image.resize((img_width, img_height))
+    pixels = asarray(im_resized)
+    img_array = tf.expand_dims(pixels, 0) # Create a batch
+    predictions = newsaved_model.predict(img_array)
+    prediction_prob=tf.nn.softmax(predictions)
+    objid=np.argmax(prediction_prob[0], axis=-1)
+    final_class = class_names[objid]
+    final_class_prob = prediction_prob[0][objid].numpy()
+    return final_class#+'  With Probability (%.2f)' %(final_class_prob)
 
 
 
